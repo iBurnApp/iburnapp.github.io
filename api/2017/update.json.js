0639f82fe4fdb1fca89cb43c5bb001fb97962f51
Chris Ballinger <chrisballinger@gmail.com>
2017-08-16 16:06:40 -0700

    Add 2017 API data

---
+++ api/2017/update.json.js
@@ -0,0 +1,10 @@
+{
+	"art": {"file": "art.json.js",
+			"updated": "2017-08-15T12:33:00-07:00"},
+	"camps": {"file": "camps.json.js",
+			  "updated": "2017-08-15T12:29:00-07:00"},
+	"events": {"file": "events.json.js",
+			   "updated": "2017-08-15T12:36:00-07:00"},
+	"points": {"file": "points.json.js",
+			  "updated": "2017-08-15T12:30:00-07:00"}
+}

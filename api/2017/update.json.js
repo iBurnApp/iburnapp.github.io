428d7768e50c1208ba2d5b3c92f03f186323b12e
Chris Ballinger <chrisballinger@gmail.com>
2017-08-16 16:22:49 -0700

    Revert "Test event and camp location update"
    This reverts commit 17b2b7f624ff6e6202a0ad45117d8f6fa5058627.

--- api/2017/update.json.js
+++ api/2017/update.json.js
@@ -2,9 +2,9 @@
 	"art": {"file": "art.json.js",
 			"updated": "2017-08-15T12:33:00-07:00"},
 	"camps": {"file": "camps.json.js",
-			  "updated": "2017-08-15T12:30:00-07:00"},
+			  "updated": "2017-08-15T12:29:00-07:00"},
 	"events": {"file": "events.json.js",
-			   "updated": "2017-08-15T12:37:00-07:00"},
+			   "updated": "2017-08-15T12:36:00-07:00"},
 	"points": {"file": "points.json.js",
 			  "updated": "2017-08-15T12:30:00-07:00"}
 }
